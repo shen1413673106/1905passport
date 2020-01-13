517f0310f4141315fb98e596a813834175884bbb
shen1413673106 <1413673106@qq.com>
2020-01-13 03:46:47 +0000

    add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    passport1905
+                    passport1905laraveldsgfsdfsfdgfdsf
                 </div>
 
                 <div class="links">
