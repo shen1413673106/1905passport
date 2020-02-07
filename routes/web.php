4776ca9e14da222798d535760a47d326534222fd
shen1413673106 <1413673106@qq.com>
2020-02-07 02:47:59 +0000

    验签

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,7 +9,32 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+////管理员模块
+//Route::prefix('api/')->middleware('CheckLogin')->group(function(){
+//    Route::post('/user/reg','User\UserController@reg');     //注册
+//    Route::post('/user/login','User\UserController@login'); // 登录
+//    Route::get('/show/time','User\UserController@showTime'); // 获取数据
+//    Route::post('/auth','User\UserController@auth'); // 鉴权
+//});
+Route::post('/api/user/reg','User\UserController@reg');     //注册
+Route::post('/api/user/login','User\UserController@login'); // 登录
+Route::get('/api/show/time','User\UserController@showTime'); // 获取数据
+Route::post('/api/auth','User\UserController@auth');
+
+Route::post('/test/reg','TestController@reg');
+Route::post('/test/login','TestController@login');
+Route::get('/test/info','TestController@info');
+
+Route::post('/test/auth','TestController@auth');
+
+
+//签名
+Route::get('/test/checksign','TestController@md5test');
+
+
+
